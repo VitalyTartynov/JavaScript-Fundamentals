d8d88b4b388fbd454837abf6ba57725405e21273
Vitaly.Tartynov <Vitaly.Tartynov@GMail.com>
2018-11-02 08:51:16 +0700

    Destructuring arrays and objects

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,3 +9,17 @@ function sendCars(...carIds) {
 }
 
 sendCars(10, 20, 30, 40);
+
+// Destructuring arrays
+let someIds = [1, 3, 5, 7, 9, 11];
+let firstId, otherIds;
+[firstId, ...otherIds] = someIds;
+console.log(firstId);
+console.log(otherIds);
+
+// Destructuring objects
+let someObject = {id: 1000, theme: 'themeValueHere'};
+let id, theme;
+({id, theme} = someObject);
+console.log(id);
+console.log(theme);
